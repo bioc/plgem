66e8cf4856f43d340ca13cb210e149b6c764ddcd
Mattia Pelizzola <mattia.pelizzola@unimib.it>
2009-02-13 05:09:46 +0000

    version 1.15.5 -added arguments 'trimAllZeroRows' and 'zeroMeanOrSD' to function 'plgem.fit' to improve fitting in cases with many zero values; -minor amendments to the vignette.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/plgem@37369 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plgem.fit.R
+++ R/plgem.fit.R
@@ -1,5 +1,7 @@
-"plgem.fit" <-
-function(data, covariateNumb=1, fit.condition=1, p=10, q=0.5, fittingEval=FALSE, plot.file=FALSE, verbose=FALSE) {
+"plgem.fit" <- function(data, covariateNumb=1, fit.condition=1, p=10, q=0.5,
+  trimAllZeroRows=FALSE, zeroMeanOrSD=c("replace", "trim"), fittingEval=FALSE,
+  plot.file=FALSE, verbose=FALSE) {
+  
 	library(Biobase)
 	library(MASS)
 
@@ -10,75 +12,120 @@ function(data, covariateNumb=1, fit.condition=1, p=10, q=0.5, fittingEval=FALSE,
 	if(class(fit.condition)!="numeric" && class(fit.condition)!="integer") stop("Argument 'fit.condition' is not of class 'numeric' or 'integer'")
 	if(class(p)!="numeric" && class(p)!="integer") stop("Argument 'p' is not of class 'numeric' or 'integer'")
 	if(class(q)!="numeric" && class(q)!="integer") stop("Argument 'q' is not of class 'numeric' or 'integer'")
-	if(!(q >= 0 && q <= 1)) stop("Argument 'q' is not in the range [0,1]")
+	if(!(q >= 0 && q <= 1)) stop("Argument 'q' is not in the range [0, 1]")
 	if(class(fittingEval)!="logical") stop("Argument 'fittingEval' is not of class 'logical'")
 	if(class(plot.file)!="logical") stop("Argument 'plot.file' is not of class 'logical'")
 	if(class(verbose)!="logical") stop("Argument 'verbose' is not of class 'logical'")
 
-	if(verbose) cat("fitting PLGEM..","\n")
+	if(verbose) cat("Fitting PLGEM...\n")
 
-	if(verbose) cat("samples extracted for fitting:","\n")
+	if(verbose) cat("samples extracted for fitting:\n")
 	condition.names <- as.character(pData(data)[,covariateNumb])
 	condition.name <- unique(condition.names)
 	data <- data[, which(condition.names==condition.name[fit.condition])]
 	if(verbose) print(pData(data))
-	row <- length(featureNames(data))
 	if(length(sampleNames(data)) < 2) stop("At least 2 replicates needed to fit PLGEM")
 
-	# 'data' mean and standard deviation
-	dataMatrix<-exprs(data)
-	data.mean<-rowMeans(dataMatrix,na.rm=TRUE)
-	data.mean<-sort(data.mean)
-	data.sd<-sd(t(dataMatrix),na.rm=TRUE)
-	data.sd<-data.sd[names(data.mean)]
-	data.mean<-replace(data.mean,data.mean<=0,min(data.mean[data.mean>0]))
-	data.sd<-replace(data.sd,data.sd<=0,min(data.sd[data.sd>0]))
+	# extract data matrix and optionally trim rows containing only zero values
+	dataMatrix <- exprs(data)
+  if (trimAllZeroRows) {
+    allZero <- apply(dataMatrix, 1, function(z) {return(all(z==0))} )
+    if (length(which(allZero))) {
+      if (verbose) cat("trimming", length(which(allZero)), "rows with only zero values.\n")
+      dataMatrix <- dataMatrix[-which(allZero), ]
+    }
+  }
+	
+	# calculate row means and standard deviations
+	stats <- as.data.frame(t(apply(dataMatrix, 1, function(x) {
+    c("mean"=mean(x, na.rm=T), "sd"=sd(x, na.rm=T))
+  })))
+  stats <- stats[order(stats$mean), ]
+  
+  # decide what to do with rows with non-positive mean or zero standard deviation
+  zeroMeanOrSD <- match.arg(zeroMeanOrSD)
+  switch(zeroMeanOrSD,
+    trim = {
+      toRemove <- union(which(stats$mean <= 0), which(stats$sd == 0))
+      if (length(toRemove) > 0) {
+        if (verbose) cat("removing", length(toRemove), "rows with non-positive mean or zero standard deviation...\n")
+        stats <- stats[-toRemove, ]  
+      }
+    },
+    replace = {
+      toRemove <- which(stats$mean <= 0)
+      if (length(toRemove) > 0) {
+        if (verbose) cat("replacing", length(toRemove), "non-positive means with smallest positive mean...\n")
+        stats$mean <- replace(stats$mean, stats$mean <= 0, min(stats$mean[stats$mean > 0]))
+      }
+      toRemove <- which(stats$sd == 0)
+      if (length(toRemove) > 0) {
+        if (verbose) cat("replacing", length(toRemove), "zero standard deviations with smallest non-zero standard deviation...\n")
+        stats$sd <- replace(stats$sd, stats$sd == 0, min(stats$sd[stats$sd > 0]))
+      }
+    }  
+  )
 
-	# Determination of Modelling Points (MP)
+	# determine modelling points (MP)
 	if(verbose) cat("determining modelling points...\n")
-	limits<-array(,dim=p+1)
-	MP.x<-array(,dim=p)
-	MP.y<-array(,dim=p)
-	for (i in 0:p) {limits[i+1]<-round(row*i/p)}
-	for (i in 1:(length(limits)-1)) {
-		MP.y[i]<-quantile(data.sd[limits[i]:limits[i+1]],q,na.rm=TRUE)	
-		MP.x[i]<-median(data.mean[limits[i]:limits[i+1]],na.rm=TRUE)
+	MP.x <- array(, dim=p)
+	MP.y <- array(, dim=p)
+	limits <- round(seq(1, nrow(stats), length.out=p + 1))
+	for (i in 1:p) {
+		MP.y[i] <- quantile(stats$sd[limits[i]:limits[i + 1]], q, na.rm=TRUE)	
+		MP.x[i] <- median(stats$mean[limits[i]:limits[i + 1]], na.rm=TRUE)
 	}
 
-	# fit of linear regression over the Modelling Points
+	# fit linear regression over the modelling points
 	if(verbose) cat("fitting data and modelling points...\n")
-	MP.lm<-lm(log(as.numeric(MP.y))~log(as.numeric(MP.x)))
-	slope<-coef(MP.lm)[2]
-	intercept<-coef(MP.lm)[1]
-	adj.r2.mp<-summary.lm(MP.lm)$adj.r.squared
-	data.pearson<-cor(log(data.mean),log(data.sd))
+	MP.lm <- lm(log(as.numeric(MP.y)) ~ log(as.numeric(MP.x)))
+	slope <- as.numeric(coef(MP.lm)[2])
+	intercept <- as.numeric(coef(MP.lm)[1])
+	adj.r2.mp <- summary.lm(MP.lm)$adj.r.squared
+	data.pearson <- cor(log(stats$mean), log(stats$sd))
 
 	if(fittingEval) {
-		if(plot.file) {png(file="fittingEval.png",width=600,height=600)}
-		# computation of model-residuals
-		modeled.spread<-(data.mean^slope)*(exp(intercept))
-		residuals<-(log(data.sd)-log(modeled.spread))
-		maxResidual<-ceiling(max(abs(residuals)))
+		if(plot.file) {png(file="fittingEval.png", width=600, height=600)}
+
+		# computation of model residuals
+		modeled.spread <- (stats$mean^slope) * (exp(intercept))
+		residuals <- (log(stats$sd) - log(modeled.spread))
+		maxResidual <- ceiling(max(abs(residuals)))
 
 		layout(matrix(1:4, 2, 2))
+		par(mar=c(3, 3, 3.5, 1), mgp=c(1.8, .6, 0))
+
 		# contour plot of data & model fit
-		mainTitle<-paste("PLGEM parameters:\nslope = ",signif(slope,3),"\nintercept = ",signif(intercept,3),sep="")
-		meanSd.kde2d<-kde2d(log(data.mean), log(data.sd),n=50)
-		contour(meanSd.kde2d,col=colors()[40:55],nlevels=15,xlab="ln(mean)",ylab="ln(sd)",cex=0.5,main=mainTitle,cex.main=0.8)
-		points(log(MP.x),log(MP.y),cex=2,col="black")
-		abline(intercept,slope,col="red",lwd=2)
+		mainTitle <- paste("PLGEM parameters:\nslope = ", signif(slope,3),
+      "\nintercept = ", signif(intercept,3), sep="")
+		meanSd.kde2d <- kde2d(log(stats$mean), log(stats$sd), n=50)
+		contour(meanSd.kde2d, col=colors()[40:55], nlevels=15, xlab="ln(mean)",
+      ylab="ln(sd)", cex=0.5, main=mainTitle, cex.main=0.8)
+		points(log(MP.x), log(MP.y), cex=2, col="black")
+		abline(intercept, slope, col="red", lwd=2)
+		xy <- par("usr")
+		text(xy[1]+0.8*diff(xy[1:2]), xy[3]+0.1*diff(xy[3:4]), paste("adj. r2 =",
+      signif(adj.r2.mp, 3), "\nPearson =", signif(data.pearson, 3)), cex=0.8)
+
 		# contour plot of residuals vs rank of mean
-		residuals.kde2d<-kde2d(rank(data.mean,ties.method="first"),residuals,n=50)
-		contour(residuals.kde2d,col=colors()[40:55],nlevels=15,xlab="Rank of mean",ylab="Residuals",ylim=c(-maxResidual,maxResidual),cex=0.5)
+		residuals.kde2d <- kde2d(rank(stats$mean,ties.method="first"), residuals, n=50)
+		contour(residuals.kde2d, col=colors()[40:55], nlevels=15,
+      xlab="Rank of mean", ylab="Residuals", ylim=c(-maxResidual,maxResidual),
+      cex=0.5)
+
 		# histogram of residuals
-		hist(residuals,breaks=100,xlab="Residuals",ylab="Counts",xlim=c(-maxResidual,maxResidual))
+		hist(residuals, breaks=100, xlab="Residuals", ylab="Counts",
+      xlim=c(-maxResidual, maxResidual))
+		box()
+
 		# qqplot of residuals distribution versus standard normal distribution
-		qqnorm(residuals,xlab="Standard Normal",ylab="Residuals",cex=0.5)
+		qqnorm(residuals, xlab="Standard Normal", ylab="Residuals", cex=0.5)
 		if(plot.file) {dev.off()}
 	}
 
 	# return model parameters
 	if(verbose) cat("done with fitting PLGEM.\n\n")
 	gc()
-	return(list(SLOPE=slope,INTERCEPT=intercept,DATA.PEARSON=data.pearson,ADJ.R2.MP=adj.r2.mp,FIT.CONDITION=fit.condition))
+	return(list(SLOPE=slope, INTERCEPT=intercept, DATA.PEARSON=data.pearson,
+    ADJ.R2.MP=adj.r2.mp, FIT.CONDITION=fit.condition))
 }
