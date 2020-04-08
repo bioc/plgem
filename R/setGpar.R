cff00c7c21441bbf64d1e8967373d1b649d1ff1d
Pavelka <pavelkan@emea.roche.com>
2020-04-08 16:05:47 +0800

    Updated class-checks in setGpar.R

--- R/setGpar.R
+++ R/setGpar.R
@@ -1,14 +1,13 @@
 setGpar <- function(minLnM=NULL, maxLnM=NULL, minLnSD=NULL, maxLnSD=NULL,
   minRes=NULL, maxRes=NULL) {
   
-  if(!missing(minLnM) && class(minLnM) != "numeric" && class(minLnM) != "integer") {
-    stop("Argument 'minLnM' is not of class 'numeric' or 'integer'")
+  if(!missing(minLnM) && !is(minLnM, "numeric") && !is(minLnM, "integer")) {
+    stop("Argument 'minLnM' is not of class 'numeric' or 'integer'.")
   }
-  if(!missing(maxLnM) && class(maxLnM) != "numeric" && class(maxLnM) != "integer") {
-    stop("Argument 'maxLnM' is not of class 'numeric' or 'integer'")
+  if(!missing(maxLnM) && !is(maxLnM, "numeric") && !is(maxLnM, "integer")) {
+    stop("Argument 'maxLnM' is not of class 'numeric' or 'integer'.")
   }
-
 
   list(minLnM=minLnM, maxLnM=maxLnM, minLnSD=minLnSD, maxLnSD=maxLnSD,
-  minRes=minRes, maxRes=maxRes)
+    minRes=minRes, maxRes=maxRes)
 }
